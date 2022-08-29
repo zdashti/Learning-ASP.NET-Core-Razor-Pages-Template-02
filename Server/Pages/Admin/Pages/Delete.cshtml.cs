45689a83a0ba7c5ae951397f19b3f0becff97ea2
Zahra Dashti <dashti.zahra@gmail.com>
2022-08-29 21:37:57 +0430

    modify delete page

--- Server/Pages/Admin/Pages/Delete.cshtml.cs
+++ Server/Pages/Admin/Pages/Delete.cshtml.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace Server.Pages.Admin.Pages;
 
@@ -11,24 +13,69 @@ public class DeleteModel : Infrastructure.BasePageModelWithDatabaseContext
         Microsoft.Extensions.Logging.ILogger<DeleteModel> logger) : base(databaseContext: databaseContext)
     {
         Logger = logger;
+
+        ViewModel = new();
     }
 
+    // **********
     private Microsoft.Extensions.Logging.ILogger<DeleteModel> Logger { get; }
+    // **********
 
-    public async System.Threading.Tasks.Task OnGetAsync(System.Guid? id)
+    // **********
+    [Microsoft.AspNetCore.Mvc.BindProperty]
+    public ViewModels.Pages.Admin.Pages.DetailsOrDeleteViewModel ViewModel { get; private set; }
+    // **********
+
+    public async System.Threading.Tasks.Task
+        <Microsoft.AspNetCore.Mvc.IActionResult> OnGetAsync(System.Guid? id)
     {
         {
             try
             {
-                if (id.HasValue)
+                if (id.HasValue == false)
                 {
+                    AddToastError
+                        (message: Resources.Messages.Errors.IdIsNull);
+
+                    return RedirectToPage(pageName: "Index");
                 }
+
+                ViewModel =
+                    await
+                    DatabaseContext.Pages
+                    .Where(current => current.Id == id.Value)
+                    .Select(current => new ViewModels.Pages.Admin.Pages.DetailsOrDeleteViewModel()
+                    {
+                        Id = current.Id,
+                        Title = current.Title,
+                        IsActive = current.IsActive,
+                        Ordering = current.Ordering,
+                        IsFeatured = current.IsFeatured,
+                        Description = current.Description,
+                        InsertDateTime = current.InsertDateTime,
+                        UpdateDateTime = current.UpdateDateTime,
+                    })
+                    .FirstOrDefaultAsync();
+
+                if (ViewModel == null)
+                {
+                    AddToastError
+                        (message: Resources.Messages.Errors.ThereIsNotAnyDataWithThisId);
+
+                    return RedirectToPage(pageName: "Index");
+                }
+
+                return Page();
             }
             catch (System.Exception ex)
             {
-                Logger.LogError(message: ex.Message);
+                Logger.LogError
+                (message: Domain.SeedWork.Constants.Logger.ErrorMessage, args: ex.Message);
 
-                AddToastError(message: Resources.Messages.Errors.UnexpectedError);
+                AddToastError
+                    (message: Resources.Messages.Errors.UnexpectedError);
+
+                return RedirectToPage(pageName: "Index");
             }
             finally
             {
@@ -42,21 +89,57 @@ public class DeleteModel : Infrastructure.BasePageModelWithDatabaseContext
     {
         try
         {
-            if (id.HasValue)
+            if (id.HasValue == false)
             {
+                AddToastError
+                    (message: Resources.Messages.Errors.IdIsNull);
+
+                return RedirectToPage(pageName: "Index");
             }
+
+            // **************************************************
+            var foundedItem =
+                await
+                DatabaseContext.Pages
+                .Where(current => current.Id == id.Value)
+                .FirstOrDefaultAsync();
+
+            if (foundedItem == null)
+            {
+                AddToastError
+                    (message: Resources.Messages.Errors.ThereIsNotAnyDataWithThisId);
+
+                return RedirectToPage(pageName: "Index");
+            }
+
+            DatabaseContext.Remove(entity: foundedItem);
+
+            await DatabaseContext.SaveChangesAsync();
+            // **************************************************
+
+            // **************************************************
+            var successMessage = string.Format
+                (Resources.Messages.Successes.Deleted,
+                Resources.DataDictionary.Page);
+
+            AddToastSuccess(message: successMessage);
+            // **************************************************
+
+            return RedirectToPage(pageName: "Index");
         }
         catch (System.Exception ex)
         {
-            Logger.LogError(message: ex.Message);
+            Logger.LogError
+                (message: Domain.SeedWork.Constants.Logger.ErrorMessage, args: ex.Message);
 
-            AddToastError(message: Resources.Messages.Errors.UnexpectedError);
+            AddToastError
+                (message: Resources.Messages.Errors.UnexpectedError);
+
+            return RedirectToPage(pageName: "Index");
         }
         finally
         {
             await DisposeDatabaseContextAsync();
         }
-
-        return Page();
     }
 }
