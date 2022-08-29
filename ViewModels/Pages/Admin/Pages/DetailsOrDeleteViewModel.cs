a242f047d3117ca2954305aef8ff8139d14115d4
ZahraDashti <dashti.zahra@gmail.com>
2022-08-29 08:41:56 +0430

    add DetailsOrDeleteViewModel

---
+++ ViewModels/Pages/Admin/Pages/DetailsOrDeleteViewModel.cs
@@ -0,0 +1,65 @@
+﻿namespace ViewModels.Pages.Admin.Pages;
+
+public class DetailsOrDeleteViewModel : UpdateViewModel
+{
+    public DetailsOrDeleteViewModel() : base()
+    {
+        InsertDateTime =
+            Domain.SeedWork.Utility.Now;
+
+        UpdateDateTime =
+            Domain.SeedWork.Utility.Now;
+    }
+
+    // **********
+    // **********
+    // **********
+    public System.DateTime InsertDateTime { get; set; }
+    // **********
+
+    // **********
+    [System.ComponentModel.DataAnnotations.Display
+    (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.InsertDateTime))]
+    public string DisplayInsertDateTime
+    {
+        get
+        {
+            var result =
+                InsertDateTime.ToString
+                    (format: Domain.SeedWork.Constants.Format.DateTime);
+
+            return result;
+        }
+    }
+    // **********
+    // **********
+    // **********
+
+    // **********
+    // **********
+    // **********
+    public System.DateTime UpdateDateTime { get; set; }
+    // **********
+    // **********
+
+    // **********
+    [System.ComponentModel.DataAnnotations.Display
+    (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.UpdateDateTime))]
+    public string DisplayUpdateDateTime
+    {
+        get
+        {
+            var result =
+                UpdateDateTime.ToString
+                    (format: Domain.SeedWork.Constants.Format.DateTime);
+
+            return result;
+        }
+    }
+    // **********
+    // **********
+    // **********
+}
+
