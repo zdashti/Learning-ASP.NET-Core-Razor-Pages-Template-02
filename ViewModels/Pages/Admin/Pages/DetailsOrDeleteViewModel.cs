91a2ea6cc2b3cc8bdcb7bbcd25e4ba99696acdde
ZahraDashti <dashti.zahra@gmail.com>
2022-09-04 08:35:40 +0430

    use new tag helper

--- ViewModels/Pages/Admin/Pages/DetailsOrDeleteViewModel.cs
+++ ViewModels/Pages/Admin/Pages/DetailsOrDeleteViewModel.cs
@@ -4,62 +4,20 @@ public class DetailsOrDeleteViewModel : UpdateViewModel
 {
     public DetailsOrDeleteViewModel() : base()
     {
-        InsertDateTime =
-            Domain.SeedWork.Utility.Now;
-
-        UpdateDateTime =
-            Domain.SeedWork.Utility.Now;
     }
 
     // **********
-    // **********
-    // **********
+    [System.ComponentModel.DataAnnotations.Display
+    (ResourceType = typeof(Resources.DataDictionary),
+        Name = nameof(Resources.DataDictionary.InsertDateTime))]
     public System.DateTime InsertDateTime { get; set; }
     // **********
 
     // **********
     [System.ComponentModel.DataAnnotations.Display
     (ResourceType = typeof(Resources.DataDictionary),
-        Name = nameof(Resources.DataDictionary.InsertDateTime))]
-    public string DisplayInsertDateTime
-    {
-        get
-        {
-            var result =
-                InsertDateTime.ToString
-                    (format: Constants.Format.DateTime);
-
-            return result;
-        }
-    }
-    // **********
-    // **********
-    // **********
-
-    // **********
-    // **********
-    // **********
-    public System.DateTime UpdateDateTime { get; set; }
-    // **********
-    // **********
-
-    // **********
-    [System.ComponentModel.DataAnnotations.Display
-    (ResourceType = typeof(Resources.DataDictionary),
         Name = nameof(Resources.DataDictionary.UpdateDateTime))]
-    public string DisplayUpdateDateTime
-    {
-        get
-        {
-            var result =
-                UpdateDateTime.ToString
-                    (format: Constants.Format.DateTime);
-
-            return result;
-        }
-    }
-    // **********
-    // **********
+    public System.DateTime UpdateDateTime { get; set; }
     // **********
 }
 
